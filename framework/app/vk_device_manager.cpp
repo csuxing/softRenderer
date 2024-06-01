184bce16092bbb32fc787475121204d477b8bde6
csuxing <1751949783@qq.com>
2024-06-01 23:20:51 +0800

    init SwapchainInfo

--- framework/app/vk_device_manager.cpp
+++ framework/app/vk_device_manager.cpp
@@ -3,6 +3,9 @@
 #include "core/error.h"
 #include "core/helpers.h"
 #include <GLFW/glfw3.h>
+#include <set>
+#include <map>
+
 namespace APP
 {
     static std::vector<const char*> stringSetToVector(const std::unordered_set<std::string>& set)
@@ -47,10 +50,16 @@ namespace APP
         }
         createInstance();
         createVKDevice();
+        createSwapchain();
     }
 
     void VkDeviceManager::createInstance()
     {
+        if (m_instanceCreated)
+        {
+            return;
+        }
+
         uint32_t apiVersion{};
         vkEnumerateInstanceVersion(&apiVersion);
         m_deviceCreationParameters.m_apiVersion = VK_API_VERSION_1_1;
@@ -66,7 +75,7 @@ namespace APP
         VK_CHECK(vkEnumerateInstanceExtensionProperties(nullptr, &instance_extension_count, nullptr));
         std::vector<VkExtensionProperties> available_instance_extensions(instance_extension_count);
         VK_CHECK(vkEnumerateInstanceExtensionProperties(nullptr, &instance_extension_count, available_instance_extensions.data()));
-        for (auto optExt : available_instance_extensions)
+        for (const auto& optExt : available_instance_extensions)
         {
             m_optionalExtension.instance.insert(optExt.extensionName);
         }
@@ -75,7 +84,7 @@ namespace APP
 
         std::vector<VkLayerProperties> supported_validation_layers(instance_layer_count);
         VK_CHECK(vkEnumerateInstanceLayerProperties(&instance_layer_count, supported_validation_layers.data()));
-        for (auto optLayer : supported_validation_layers)
+        for (const auto& optLayer : supported_validation_layers)
         {
             m_optionalExtension.layers.insert(optLayer.layerName);
         }
@@ -112,6 +121,7 @@ namespace APP
         }
         VK_CHECK(vkCreateInstance(&instance_info, nullptr, &m_instance));
         volkLoadInstance(m_instance);
+        m_instanceCreated = true;
     }
 
     void VkDeviceManager::createVKDevice()
@@ -119,6 +129,106 @@ namespace APP
         queryGpu();
         createSurface();
         pickPhysicalDevice();
+        findQueueFamilies();
+        createLogicalDevice();
+        initVma();
+    }
+
+    void VkDeviceManager::createSwapchain()
+    {
+        // todo(xingyuhang) : destory old swapchain
+        if (m_swapchainInfo.handle != VK_NULL_HANDLE)
+        {
+            uint32_t imageCount = to_u32(m_swapchainInfo.imageViews.size());
+            for (uint32_t i = 0; i < imageCount; i++)
+            {
+                vkDestroyImageView(m_logicalDevice, m_swapchainInfo.imageViews[i], nullptr);
+            }
+            vkDestroySwapchainKHR(m_logicalDevice, m_swapchainInfo.handle, nullptr);
+        }
+        // todo(xingyuhang) : select format and present mode more perfect
+        VkPhysicalDevice physicalDeviceHnadle = getCurrentUseGpu().handle;
+        uint32_t surfaceFormatCount;
+        vkGetPhysicalDeviceSurfaceFormatsKHR(physicalDeviceHnadle, m_surface, &surfaceFormatCount, nullptr);
+        std::vector<VkSurfaceFormatKHR> formatKHRs;
+        formatKHRs.resize(surfaceFormatCount);
+        vkGetPhysicalDeviceSurfaceFormatsKHR(physicalDeviceHnadle, m_surface, &surfaceFormatCount, formatKHRs.data());
+        VkSurfaceFormatKHR temp = m_swapchainInfo.format;
+        if (std::find_if(formatKHRs.begin(), formatKHRs.end(), [temp](VkSurfaceFormatKHR input) { 
+            if (input.format == temp.format && input.colorSpace == temp.colorSpace) 
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            }) == formatKHRs.end())
+        {
+            m_swapchainInfo.format = *formatKHRs.begin();
+        }
+
+        uint32_t surfacePresentModeCount;
+        vkGetPhysicalDeviceSurfacePresentModesKHR(physicalDeviceHnadle, m_surface, &surfacePresentModeCount, nullptr);
+        std::vector<VkPresentModeKHR> modes;
+        modes.resize(surfacePresentModeCount);
+        vkGetPhysicalDeviceSurfacePresentModesKHR(physicalDeviceHnadle, m_surface, &surfaceFormatCount, modes.data());
+
+        if (std::find(modes.begin(), modes.end(), m_swapchainInfo.presentMode) == modes.end())
+        {
+            m_swapchainInfo.presentMode = *modes.begin();
+        }
+
+        VkSurfaceCapabilitiesKHR capablities;
+        vkGetPhysicalDeviceSurfaceCapabilitiesKHR(physicalDeviceHnadle, m_surface, &capablities);
+
+        VkSwapchainCreateInfoKHR swapchainCreateInfo;
+        swapchainCreateInfo.sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR;
+        swapchainCreateInfo.pNext = nullptr;
+        swapchainCreateInfo.flags = 0;
+        swapchainCreateInfo.surface = m_surface;
+        swapchainCreateInfo.minImageCount = capablities.minImageCount + 1;
+        swapchainCreateInfo.imageFormat = m_swapchainInfo.format.format;
+        swapchainCreateInfo.imageColorSpace = m_swapchainInfo.format.colorSpace;
+        swapchainCreateInfo.imageExtent = capablities.currentExtent;
+        swapchainCreateInfo.imageArrayLayers = 1;
+        swapchainCreateInfo.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
+        swapchainCreateInfo.imageSharingMode = VK_SHARING_MODE_EXCLUSIVE;
+        swapchainCreateInfo.queueFamilyIndexCount = 0;
+        swapchainCreateInfo.pQueueFamilyIndices = nullptr;
+        swapchainCreateInfo.preTransform = VK_SURFACE_TRANSFORM_IDENTITY_BIT_KHR;
+        swapchainCreateInfo.compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR;
+        swapchainCreateInfo.presentMode = m_swapchainInfo.presentMode;
+        swapchainCreateInfo.clipped = VK_TRUE;
+        swapchainCreateInfo.oldSwapchain = m_swapchainInfo.handle;
+        VK_CHECK(vkCreateSwapchainKHR(m_logicalDevice, &swapchainCreateInfo, nullptr, &m_swapchainInfo.handle));
+
+        uint32_t imageCount{};
+        vkGetSwapchainImagesKHR(m_logicalDevice, m_swapchainInfo.handle, &imageCount, nullptr);
+        m_swapchainInfo.images.resize(imageCount);
+        vkGetSwapchainImagesKHR(m_logicalDevice, m_swapchainInfo.handle, &imageCount, m_swapchainInfo.images.data());
+        // create image views
+        m_swapchainInfo.imageViews.resize(imageCount);
+        for (uint32_t i = 0; i < imageCount; i++)
+        {
+            VkImageViewCreateInfo imageViewCreateInfo{};
+            imageViewCreateInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+            imageViewCreateInfo.pNext = nullptr;
+            imageViewCreateInfo.flags = 0;
+            imageViewCreateInfo.image = m_swapchainInfo.images[i];
+            imageViewCreateInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+            imageViewCreateInfo.format = m_swapchainInfo.format.format;
+            imageViewCreateInfo.components.r = VK_COMPONENT_SWIZZLE_R;
+            imageViewCreateInfo.components.g = VK_COMPONENT_SWIZZLE_G;
+            imageViewCreateInfo.components.b = VK_COMPONENT_SWIZZLE_B;
+            imageViewCreateInfo.components.a = VK_COMPONENT_SWIZZLE_A;
+            imageViewCreateInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+            imageViewCreateInfo.subresourceRange.layerCount = 1;
+            imageViewCreateInfo.subresourceRange.baseArrayLayer = 0;
+            imageViewCreateInfo.subresourceRange.levelCount = 1;
+            imageViewCreateInfo.subresourceRange.baseMipLevel = 0;
+            VK_CHECK(vkCreateImageView(m_logicalDevice, &imageViewCreateInfo, nullptr, &m_swapchainInfo.imageViews[i]));
+        }
     }
 
     void VkDeviceManager::queryGpu()
@@ -185,6 +295,7 @@ namespace APP
                     if (present_supported)
                     {
                         m_currentGpuIndex = i;
+                        LOG_INFO("select physical device gpu : {}", gpu.properties.deviceName);
                         break;
                     }
                 }
@@ -199,10 +310,143 @@ namespace APP
         {
             m_optionalExtension.device.insert(ext.extensionName);
         }
+
+        for (const auto& ext : m_deviceCreationParameters.requiredVulkanDeviceExtensions)
+        {
+            m_requiredExtension.device.insert(ext);
+        }
+
+        for (const auto& needNext : m_requiredExtension.device)
+        {
+            if (m_optionalExtension.device.find(needNext) != m_optionalExtension.device.end())
+            {
+                LOG_INFO("required device extension : {}", needNext);
+            }
+            else
+            {
+                LOG_ERROR("device extension : {} is not find!", needNext);
+            }
+        }
+    }
+
+    void VkDeviceManager::findQueueFamilies()
+    {
+        const GpuInfo& GPU = getCurrentUseGpu();
+        auto& props = GPU.queueFamilyProperties;
+        size_t familyCount = props.size();
+        // fix me : queue family is different between them
+        for (int i = 0; i < familyCount; ++i)
+        {
+            auto& queueFamilyProperty = props[i];
+
+            if (queueFamilyProperty.queueCount < 1)
+            {
+                continue;
+            }
+
+            if (m_GraphicsQueueFamily == -1)
+            {
+                if (queueFamilyProperty.queueFlags & VK_QUEUE_GRAPHICS_BIT)
+                {
+                    m_GraphicsQueueFamily = i;
+                }
+            }
+
+            if (m_ComputeQueueFamily == -1)
+            {
+                if (!(queueFamilyProperty.queueFlags & VK_QUEUE_GRAPHICS_BIT) &&
+                    queueFamilyProperty.queueFlags & VK_QUEUE_COMPUTE_BIT)
+                {
+                    m_ComputeQueueFamily = i;
+                }
+            }
+            
+            if (m_TransferQueueFamily == -1)
+            {
+                if (!(queueFamilyProperty.queueFlags & VK_QUEUE_GRAPHICS_BIT) &&
+                    !(queueFamilyProperty.queueFlags & VK_QUEUE_COMPUTE_BIT) &&
+                    (queueFamilyProperty.queueFlags & VK_QUEUE_TRANSFER_BIT))
+                {
+                    m_TransferQueueFamily = i;
+                }
+            }
+
+            if (m_PresentQueueFamily == -1)
+            {
+                if (glfwGetPhysicalDevicePresentationSupport(m_instance, GPU.handle, i))
+                {
+                    m_PresentQueueFamily = i;
+                }
+            }
+        }
+
+        ASSERT(m_GraphicsQueueFamily != -1);
+        ASSERT(m_ComputeQueueFamily != -1);
+        ASSERT(m_TransferQueueFamily != -1);
+        ASSERT(m_PresentQueueFamily != -1);
+
+        LOG_INFO("Graphics queue family index : {}", m_GraphicsQueueFamily);
+        LOG_INFO("Compute queue family index  : {}", m_ComputeQueueFamily);
+        LOG_INFO("Transfer queue family index : {}", m_TransferQueueFamily);
+        LOG_INFO("Present queue family index  : {}", m_PresentQueueFamily);
     }
 
     void VkDeviceManager::createLogicalDevice()
     {
+        VkDeviceCreateInfo deviceCrateInfo{};
+        deviceCrateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+        deviceCrateInfo.flags = 0;
+        deviceCrateInfo.pNext = nullptr;
+
+        std::vector<VkDeviceQueueCreateInfo> queueCreateInfos;
+        std::set<int> queueFamilyIndexs;
+        queueFamilyIndexs.insert(m_GraphicsQueueFamily);
+        queueFamilyIndexs.insert(m_ComputeQueueFamily);
+        queueFamilyIndexs.insert(m_TransferQueueFamily);
+        queueFamilyIndexs.insert(m_PresentQueueFamily);
+
+        std::map<int, std::vector<float>> queuePriorities;
+        for (int index : queueFamilyIndexs)
+        {
+            auto& properties = getCurrentUseGpu().queueFamilyProperties[index];
+            queueCreateInfos.push_back({});
+            auto& queueCreateInfo = queueCreateInfos.back();
+            queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+            queueCreateInfo.flags = 0;
+            queueCreateInfo.pNext = nullptr;
+            queueCreateInfo.queueFamilyIndex = index;
+            queueCreateInfo.queueCount = properties.queueCount;
+            std::vector<float> priorities;
+            for (uint32_t i = 0; i < properties.queueCount; ++i)
+            {
+                priorities.push_back(1.f);
+            }
+            queuePriorities.insert({ index, priorities });
+            queueCreateInfo.pQueuePriorities = queuePriorities[index].data();
+        }
+
+        auto extensions = stringSetToVector(m_requiredExtension.device);
+
+        deviceCrateInfo.queueCreateInfoCount = to_u32(queuePriorities.size());
+        deviceCrateInfo.pQueueCreateInfos = queueCreateInfos.data();
+        deviceCrateInfo.enabledLayerCount = 0;
+        deviceCrateInfo.ppEnabledLayerNames = nullptr;
+        deviceCrateInfo.enabledExtensionCount = to_u32(extensions.size());
+        deviceCrateInfo.ppEnabledExtensionNames = extensions.data();
+        deviceCrateInfo.pEnabledFeatures = &m_physicalDevicefeatures;
+        VK_CHECK(vkCreateDevice(getCurrentUseGpu().handle, &deviceCrateInfo, nullptr, &m_logicalDevice));
+        LOG_INFO("create logical device successful!");
+
+        // get vkqueue handle
+        vkGetDeviceQueue(m_logicalDevice, m_GraphicsQueueFamily, 0, &m_GraphicsQueue);
+        vkGetDeviceQueue(m_logicalDevice, m_ComputeQueueFamily, 0, &m_ComputeQueue);
+        vkGetDeviceQueue(m_logicalDevice, m_TransferQueueFamily, 0, &m_TransferQueue);
+        vkGetDeviceQueue(m_logicalDevice, m_PresentQueueFamily, 0, &m_PresentQueue);
+
+        ASSERT(m_GraphicsQueue != VK_NULL_HANDLE);
+        ASSERT(m_ComputeQueue != VK_NULL_HANDLE);
+        ASSERT(m_TransferQueue != VK_NULL_HANDLE);
+        ASSERT(m_PresentQueue != VK_NULL_HANDLE);
     }
 
     bool VkDeviceManager::validateInstanceExtensionAndLayer()
@@ -234,5 +478,42 @@ namespace APP
         }
 
         return true;
+    }
+    void VkDeviceManager::initVma()
+    {
+        // init vma
+        VmaVulkanFunctions vma_vulkan_func{};
+        vma_vulkan_func.vkAllocateMemory = vkAllocateMemory;
+        vma_vulkan_func.vkBindBufferMemory = vkBindBufferMemory;
+        vma_vulkan_func.vkBindImageMemory = vkBindImageMemory;
+        vma_vulkan_func.vkCreateBuffer = vkCreateBuffer;
+        vma_vulkan_func.vkCreateImage = vkCreateImage;
+        vma_vulkan_func.vkDestroyBuffer = vkDestroyBuffer;
+        vma_vulkan_func.vkDestroyImage = vkDestroyImage;
+        vma_vulkan_func.vkFlushMappedMemoryRanges = vkFlushMappedMemoryRanges;
+        vma_vulkan_func.vkFreeMemory = vkFreeMemory;
+        vma_vulkan_func.vkGetBufferMemoryRequirements = vkGetBufferMemoryRequirements;
+        vma_vulkan_func.vkGetImageMemoryRequirements = vkGetImageMemoryRequirements;
+        vma_vulkan_func.vkGetPhysicalDeviceMemoryProperties = vkGetPhysicalDeviceMemoryProperties;
+        vma_vulkan_func.vkGetPhysicalDeviceProperties = vkGetPhysicalDeviceProperties;
+        vma_vulkan_func.vkInvalidateMappedMemoryRanges = vkInvalidateMappedMemoryRanges;
+        vma_vulkan_func.vkMapMemory = vkMapMemory;
+        vma_vulkan_func.vkUnmapMemory = vkUnmapMemory;
+        vma_vulkan_func.vkCmdCopyBuffer = vkCmdCopyBuffer;
+
+        vma_vulkan_func.vkGetInstanceProcAddr = vkGetInstanceProcAddr;
+        vma_vulkan_func.vkGetDeviceProcAddr = vkGetDeviceProcAddr;
+
+        VmaAllocatorCreateInfo allocator_info{};
+        allocator_info.physicalDevice = getCurrentUseGpu().handle;
+        allocator_info.device = m_logicalDevice;
+        allocator_info.instance = m_instance;
+
+        allocator_info.pVulkanFunctions = &vma_vulkan_func;
+        VkResult result = vmaCreateAllocator(&allocator_info, &m_memoryAllocator);
+        if (result != VK_SUCCESS)
+        {
+            throw std::runtime_error{ "Cannot create allocator" };
+        }
     }
 }
--- framework/app/vk_device_manager.h
+++ framework/app/vk_device_manager.h
@@ -4,6 +4,7 @@
 #include <unordered_set>
 #include <vector>
 #include <volk.h>
+#include "vk_mem_alloc.h"
 namespace APP
 {
     struct VulkanExtensionSet
@@ -15,10 +16,18 @@ namespace APP
     struct GpuInfo
     {
         VkPhysicalDevice                        handle;
-        VkPhysicalDeviceFeatures                features{};
+        VkPhysicalDeviceFeatures                features;
         VkPhysicalDeviceProperties              properties;
         VkPhysicalDeviceMemoryProperties        memoryProperties;
         std::vector<VkQueueFamilyProperties>    queueFamilyProperties;
+    };
+    struct SwapchainInfo
+    {
+        VkSurfaceFormatKHR          format{ VK_FORMAT_B8G8R8A8_UNORM, VK_COLOR_SPACE_SRGB_NONLINEAR_KHR };
+        VkPresentModeKHR            presentMode{ VK_PRESENT_MODE_MAILBOX_KHR };
+        VkSwapchainKHR              handle{ VK_NULL_HANDLE };
+        std::vector<VkImage>        images;
+        std::vector<VkImageView>    imageViews;
     };
     class VkDeviceManager : public DeviceManager
     {
@@ -27,12 +36,16 @@ namespace APP
     protected:
         void createInstance();
         void createVKDevice();
+        void createSwapchain();
+
         const GpuInfo& getCurrentUseGpu() const { return m_gpus[m_currentGpuIndex]; }
 
         void queryGpu();
         void createSurface();
         void pickPhysicalDevice();
+        void findQueueFamilies();
         void createLogicalDevice();
+        void initVma();
     private:
         bool validateInstanceExtensionAndLayer();
 
@@ -42,16 +55,25 @@ namespace APP
         VulkanExtensionSet              m_requiredExtension;
         VulkanExtensionSet              m_optionalExtension; // instance extensions && layer && device extensions in current PC
         std::vector<GpuInfo>            m_gpus;
+        // todo(xingyuhang) : if you want to use new feratures, please set this, set in m_deviceCreationParameters
+        VkPhysicalDeviceFeatures        m_physicalDevicefeatures;
 
         VkInstance                      m_instance{ VK_NULL_HANDLE };
-        VkDevice                        m_VulkanDevice{VK_NULL_HANDLE};
+        VkDevice                        m_logicalDevice{VK_NULL_HANDLE};
         VkSurfaceKHR                    m_surface;
         uint32_t                        m_currentGpuIndex{};
+        SwapchainInfo                   m_swapchainInfo{};
 
         VkQueue                         m_GraphicsQueue{VK_NULL_HANDLE};
         VkQueue                         m_ComputeQueue{VK_NULL_HANDLE};
         VkQueue                         m_TransferQueue{VK_NULL_HANDLE};
         VkQueue                         m_PresentQueue{VK_NULL_HANDLE};
+
+        int                             m_GraphicsQueueFamily = -1;
+        int                             m_ComputeQueueFamily = -1;
+        int                             m_TransferQueueFamily = -1;
+        int                             m_PresentQueueFamily = -1;
+        VmaAllocator                    m_memoryAllocator{ VK_NULL_HANDLE };
     };
 }
 
